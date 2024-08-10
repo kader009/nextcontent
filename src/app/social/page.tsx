40d4453970bf27e9c5a5f2410adca94936c915d2
kader009 <kadermollarasel000@gmail.com>
2024-08-10 16:54:03 +0600

    github auth modify

---
+++ src/app/social/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SocialLogin = () => {
+  return (
+    <div >
+      <h1 className='text-center text-2xl mt-5 font-bold'>Welcome to social log</h1>
+      <br />
+      <br />
+      
+    </div>
+  );
+};
+
+export default SocialLogin;
